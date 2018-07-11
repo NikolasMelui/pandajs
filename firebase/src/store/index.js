12549f33722245606aff811029b1540023c20e16
NikolasMelui <slak@samaradom.ru>
2018-07-11 21:03:06 +0400

    Add send function with createUser functionality.

--- firebase/src/store/index.js
+++ firebase/src/store/index.js
@@ -1,18 +1,27 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import firebase from '../firebase/index.js';
+
 Vue.use(Vuex);
 
 const store = () =>
-  new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {
-      send({ commit }, data) {
-        console.log(data);
-      },
-    },
-    getters: {},
-  });
+	new Vuex.Store({
+		state: {},
+		mutations: {},
+		actions: {
+			send({ commit }, data) {
+				firebase
+					.auth()
+					.createUserWithEmailAndPassword(data.email, data.password)
+					.then(userkey => {
+						console.log(userkey);
+					})
+					.catch(err => console.log(err));
+				console.log(data);
+			},
+		},
+		getters: {},
+	});
 
 export default store;
